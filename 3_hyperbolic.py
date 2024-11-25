309140f392c2e60bf0d3ae4baf719555820aff32
Y.D.X. <73375426+YDX-2147483647@users.noreply.github.com>
2024-11-25 17:15:45 +0800

    feat: 迎风格式

--- 3_hyperbolic.py
+++ 3_hyperbolic.py
@@ -25,10 +25,10 @@ def __():
 @app.cell
 def __():
     import numpy as np
-    from numpy import linalg
+    from numpy import linalg, newaxis
 
     np.set_printoptions(precision=3, suppress=True)
-    return linalg, np
+    return linalg, newaxis, np
 
 
 @app.cell
@@ -39,8 +39,20 @@ def __():
 
 @app.cell
 def __():
-    from util import typst
-    return (typst,)
+    from typing import override
+    return (override,)
+
+
+@app.cell
+def __():
+    from hyperbolic_pde import Solver, benchmark, ref
+    return Solver, benchmark, ref
+
+
+@app.cell
+def __():
+    from util import multi_diag, plot_surface, typst
+    return multi_diag, plot_surface, typst
 
 
 @app.cell(hide_code=True)
@@ -49,13 +61,19 @@ def __(mo):
     return
 
 
+@app.cell
+def __():
+    a = 5
+    return (a,)
+
+
 @app.cell(hide_code=True)
-def __(typst):
-    typst(r"""
+def __(a, typst):
+    typst(rf"""
     #import "@preview/physica:0.9.3": pdv, eval
 
     $
-    pdv(u,t) + 5 pdv(u,x) = 0, x in RR. \
+    pdv(u,t) + {a} pdv(u,x) = 0, x in RR. \
     eval(u)_(t=0) = cases(
       2 &"if" x in (-1,0),
       1 &"if" x = 0,
@@ -64,6 +82,125 @@ def __(typst):
     ).
     $
     """)
+    return
+
+
+@app.cell(hide_code=True)
+def __(mo):
+    dx = mo.ui.slider(
+        0.06, 0.5, 0.04, label=r"$\mathrm{d} x$", show_value=True, debounce=True
+    )
+    dx
+    return (dx,)
+
+
+@app.cell
+def __():
+    r = 1 / 6
+    return (r,)
+
+
+@app.cell(hide_code=True)
+def __(dx, mo, r):
+    dt = r * dx.value
+    mo.md(rf"$\mathrm{{d}}t = {dt}$.")
+    return (dt,)
+
+
+@app.cell
+def __(dx, np):
+    x = np.arange(-5, 5 + dx.value, dx.value)
+    return (x,)
+
+
+@app.cell
+def __(dt, np):
+    t = np.arange(0, 1.0, dt)
+    return (t,)
+
+
+@app.cell
+def __(plot_surface, ref, t, x):
+    plot_surface(t, x, ref(t, x), title="真解")
+    return
+
+
+@app.cell
+def __(np, t, x):
+    benchmark_kwargs = dict(
+        x_max=x[-1],
+        x_min=x[0],
+        t_max=x[-1],
+        t_min=t[0],
+        dt_dx_list=[(_dx / 6, _dx) for _dx in 2.0 ** np.arange(-6, -2, 1)],
+    )
+    return (benchmark_kwargs,)
+
+
+@app.cell(hide_code=True)
+def __(mo):
+    mo.md(r"""## 1 迎风""")
+    return
+
+
+@app.cell
+def __(Solver, a, multi_diag, np, override):
+    class SolverWind(Solver):
+        @override
+        def post_init(self) -> None:
+            # to_next_u[#next_x, #current_x]
+            self.to_next_u = (
+                np.eye(self.x.size)
+                + a * self.dt * multi_diag([1, -1, 0], size=self.x.size) / self.dx
+            )
+
+        @override
+        def step(self, t) -> None:
+            self.u[:, t] = self.to_next_u @ self.u[:, t - 1]
+    return (SolverWind,)
+
+
+@app.cell
+def __(SolverWind, t, x):
+    solver_wind = SolverWind(t=t, x=x)
+    solver_wind.solve()
+    solver_wind.to_next_u
+    return (solver_wind,)
+
+
+@app.cell
+def __(plot_surface, solver_wind, t, x):
+    plot_surface(t, x, solver_wind.u, title="近似解")
+    return
+
+
+@app.cell
+def __(plot_surface, solver_wind, t, x):
+    plot_surface(t, x, solver_wind.error(), title="误差")
+    return
+
+
+@app.cell
+def __(solver_wind):
+    solver_wind.max_error()
+    return
+
+
+@app.cell
+def __(SolverWind, benchmark, benchmark_kwargs):
+    benchmark(SolverWind, **benchmark_kwargs)
+    return
+
+
+@app.cell(hide_code=True)
+def __(mo):
+    mo.md(
+        r"""
+        ## 2 蛙跳
+
+        需要虚拟网格。
+        """
+    )
     return
 
 
---
+++ hyperbolic_pde.py
@@ -0,0 +1,151 @@
+from __future__ import annotations
+
+from abc import ABC, abstractmethod
+from collections import deque
+from typing import TYPE_CHECKING
+
+import marimo as mo
+import numpy as np
+from numpy import newaxis
+from pandas import DataFrame
+
+from parabolic_pde import _Solvable
+
+if TYPE_CHECKING:
+    from typing import Collection, Final
+
+
+def setup_conditions(t: np.ndarray, x: np.ndarray) -> np.ndarray:
+    """根据初始条件、边界条件准备预备解
+
+    Params:
+        t[#t]
+        x[#x]
+    Returns:
+        u[#x, #t]
+    """
+    assert t.ndim == 1
+    assert x.ndim == 1
+
+    u = np.zeros((x.size, t.size))
+    u[:, 0] = (-1 < x) * (x < 0) * 2 - (0 < x) * (x < 1) * 2
+
+    return u
+
+
+@mo.cache
+def ref(t: np.ndarray, x: np.ndarray) -> np.ndarray:
+    """真解
+
+    Params:
+        t[#t]
+        x[#x]
+    Returns:
+        u[#x, #t]
+    """
+    assert t.ndim == 1
+    assert x.ndim == 1
+    x_initial = x[:, newaxis] - 5 * t[newaxis, :]
+    return (-1 < x_initial) * (x_initial < 0) * 2 - (0 < x_initial) * (
+        x_initial < 1
+    ) * 2
+
+
+class _PerformanceMixin(_Solvable):
+    def error(self) -> np.ndarray:
+        return self.u - ref(self.t, self.x)
+
+    def max_error(self) -> float:
+        return np.abs(self.error()).max()
+
+
+class Solver(_PerformanceMixin, ABC):
+    """PDE solver
+
+    init (and post_init) → solve
+    """
+
+    dx: Final[float]
+    dt: Final[float]
+
+    # x[#x]
+    x: Final[np.ndarray]
+
+    # t[#t]
+    t: Final[np.ndarray]
+
+    # u[#x, #t]
+    u: np.ndarray
+
+    def __init__(self, *, t: np.ndarray, x: np.ndarray) -> None:
+        assert x.ndim == 1
+        assert t.ndim == 1
+
+        self.dt = np.diff(t).mean()
+        self.dx = np.diff(x).mean()
+        self.t = t
+        self.x = x
+
+        self.u = setup_conditions(t, x)
+
+        self.post_init()
+
+    def post_init(self) -> None:
+        """Prepare after `__init__`"""
+        pass
+
+    @abstractmethod
+    def step(self, t: int) -> None:
+        """Update u[1:-1, t]"""
+        pass
+
+    def solve(self) -> None:
+        """Solve u"""
+        for t in range(self.t.size):
+            if t == 0:
+                continue
+            self.step(t)
+
+    def validate(self, t: int) -> None:
+        """Validate the PDE at `t`
+
+        An optional abstract method.
+
+        Raise an `AssertionError` if invalid.
+        """
+        pass
+
+
+def benchmark(
+    solver_cls: type[Solver],
+    *,
+    t_min: float,
+    t_max: float,
+    x_min: float,
+    x_max: float,
+    dt_dx_list: Collection[tuple[float, float]],
+) -> DataFrame:
+    """Benchmark
+
+    Returns:
+        列为dx、最大误差、时长
+    """
+    assert issubclass(solver_cls, Solver)
+
+    # (dt, dx, max_error)[]
+    stat: deque[tuple[float, float, float]] = deque()
+
+    for dt, dx in mo.status.progress_bar(dt_dx_list):  # type: ignore
+        dt: float
+        dx: float
+        x = np.arange(x_min, x_max + dx, dx)
+        t = np.arange(t_min, t_max + dt, dt)
+
+        solver = solver_cls(x=x, t=t)
+        solver.solve()
+        stat.append((dt, dx, solver.max_error()))
+
+    return DataFrame(
+        [[dt, dx, error] for (dt, dx, error) in stat],
+        columns=["dt", "dx", "最大误差"],
+    )
